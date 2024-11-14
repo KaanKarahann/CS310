5d2a04895d03515f1d8f8164b773a3beb1e491a0
Kaan Karahan <143435367+KaanKarahann@users.noreply.github.com>
2024-11-14 19:10:20 +0300

    initial commit

---
+++ CS310Project/src/test/java/edu/sabanciuniv/cs310project/Cs310ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.cs310project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs310ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
